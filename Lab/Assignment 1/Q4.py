62ad9c9316ea07d46c59fa507fb483b23cd197ed
Harsh100012 <153731042+Harsh100012@users.noreply.github.com>
2024-07-23 09:32:56 +0530

    Add files via upload

---
+++ Lab/Assignment 1/Q4.py
@@ -0,0 +1,13 @@
+def fileinfo():
+    words = 0
+    with open("story.txt","r") as f:
+        lines = f.readlines()
+        for line in lines:
+            words = line.split()
+            for word in words:
+              if(len(word) < 4):
+                 print(word, end=" ")
+
+fileinfo()
+
+
